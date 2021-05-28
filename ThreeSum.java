513b60ba332f6680450f511b753a646197cfab11
unknown <henry.david.zhu@gmail.com>
2021-05-27 23:16:18 -0700

    Added Three Sum Solution

---
+++ ThreeSum.java
@@ -0,0 +1,77 @@
+class Solution {
+    List<List<Integer>> tripletList = new LinkedList<List<Integer>>();
+    
+    // Find all non-duplicate triplets that sums up to 0
+    public List<List<Integer>> threeSum(int[] nums) {
+        if (nums.length == 0) {
+            return tripletList;
+        }
+        
+        Arrays.sort(nums);
+        
+        int prevNum = nums[0];
+        
+        // Iterate through all possible first (base) numbers in our triplet
+        for (int i = 0; i < (nums.length - 2); i++) {
+            int currNum = nums[i];
+
+            // Only analyze the current number if we haven't processed it already
+            if (currNum != prevNum || i == 0) {
+                findCorrespondingPair(nums, i);
+            }
+            
+            prevNum = currNum;
+        }
+        
+        return tripletList;
+    }
+    
+    public void findCorrespondingPair(int[] numbers, int firstNumIndex) {
+        int firstNum = numbers[firstNumIndex];
+        int target = 0 - firstNum;
+        
+        int leftNumIndex = firstNumIndex + 1;
+        int rightNumIndex = numbers.length - 1;
+        
+        // While we still have a range to search for
+        while (leftNumIndex < rightNumIndex) {
+            int leftNum = numbers[leftNumIndex];
+            int rightNum = numbers[rightNumIndex];
+            int sum = leftNum + rightNum;
+            
+            // If we have already found the element
+            if (sum == target) {
+                // Compute the triplet's values
+                List<Integer> triplet = new LinkedList<Integer>();
+                triplet.add(firstNum);
+                triplet.add(leftNum);
+                triplet.add(rightNum);
+                
+                // Add our triplet to our list of triplets
+                tripletList.add(triplet);
+
+                // Decrease our search range:
+                // Move the left index to point to the next number that is not 
+                // equal to the current left number (avoid duplicates)
+                leftNumIndex++;
+                
+                while (leftNumIndex < rightNumIndex && numbers[leftNumIndex] == numbers[leftNumIndex - 1]) {
+                    leftNumIndex++;
+                }
+                
+                // Decrement the right pointer
+                rightNumIndex--;
+            }
+            
+            // Our sum is too small, increment the left pointer
+            if (sum < target) {
+                leftNumIndex++;
+            }
+            
+            // Our sum is too great, decrement the right pointer
+            if (sum > target) {
+                rightNumIndex--;
+            }
+        }
+    }
+}
