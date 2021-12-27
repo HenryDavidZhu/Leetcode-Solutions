772cf8ca2d152b204f5d671f049f11f1845deb4f
Henry Zhu <henry.david.zhu@gmail.com>
2021-12-27 15:58:22 -0800

    Added 2 Sum Solution.

--- TwoSum.java
+++ TwoSum.java
@@ -1,16 +1,19 @@
+// O(n) time, O(n) space
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        // key + nums[value] = target
-        Map<Integer, Integer> differenceHash = new HashMap<Integer, Integer>();
-
-        for (int i = 0; i < nums.length; i++) {
-            // See if we have found the pair from our mapping and that we don't use the
-            // same element.
-            if (differenceHash.containsKey(nums[i]) && differenceHash.get(nums[i]) != i) {
-                return new int[] {differenceHash.get(nums[i]), i};
+        // Maps each value in the array to its corresponding index.
+        Map<Integer, Integer> differenceMap = new HashMap<Integer, Integer>();
+       
+        for (int secondIndex = 0; secondIndex < nums.length; secondIndex++) {
+            int secondNum = nums[secondIndex];
+            
+            // See if we have located the corresponding first number of the pair
+            // in our map (target - secondNum).
+            if (differenceMap.containsKey(target - secondNum)) {
+                return new int[] {differenceMap.get(target - secondNum), secondIndex};
             }
             
-            differenceHash.put(target - nums[i], i);
+            differenceMap.put(secondNum, secondIndex);
         }
         
         return new int[] {-1, -1};
